11c29eadf986ef66674e74dc9369e90db12208b4
E-Ozdemir <emreclarusway@gmail.com>
2021-10-21 20:46:35 +0200

    class based views

---
+++ Backend Module/Backend Inclass/Class_Based_Views/fscohort/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import StudentListView, home, student_add, student_detail, student_update,student_delete
+from django.views.generic import TemplateView
+from .views import HomeView,StudentListView, StudentDetailView,StudentUpdateView
+
+urlpatterns = [
+    # path('', home, name="home"),
+    # path('', TemplateView.as_view(template_name='fscohort/home.html'), name='home'),
+    path('', HomeView.as_view(), name='home'),
+    # path('student_list/', student_list, name="list"),
+    path('student_list/', StudentListView.as_view(), name="list"),
+    path('student_add/', student_add, name="add"),
+    # path('detail/<int:id>/', student_detail, name="detail"),
+    path('detail/<int:id>/', StudentDetailView.as_view(), name="detail"),
+    # path('update/<int:id>/', student_update, name="update"),
+    path('update/<int:pk>/', StudentUpdateView.as_view(), name="update"),
+    path('delete/<int:id>/', student_delete, name="delete"),
+]  
---
+++ Backend Module/Backend Inclass/Class_Based_Views/fscohort/views.py
@@ -0,0 +1,114 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .forms import StudentForm
+from .models import Student
+from django.views.generic import TemplateView,ListView, DetailView,CreateView,UpdateView
+from django.urls import reverse_lazy
+
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, "fscohort/home.html")
+
+class HomeView(TemplateView):
+    template_name = 'fscohort/home.html'
+
+
+def student_list(request):
+    
+    students = Student.objects.all()
+    
+    context = {
+        "students":students
+    }
+    
+    return render(request, "fscohort/student_list.html", context)
+
+class StudentListView(ListView):
+    model = Student
+    # default name app/modelname_List.html
+    # template_name = 'fscohort/student_list.html'
+    context_object_name = 'students' #default name object_list
+    paginate_by = 5
+
+def student_add(request):
+    form = StudentForm()
+    
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+            
+    
+    context = {
+       
+       "form":form     
+    }
+    
+    return render(request, "fscohort/student_add.html", context)
+
+class StudentCreateView(CreateView):
+    model = Student  
+    form_class = StudentForm
+    template_name = 'fscohort/student_add.html' # default name 
+    success_url =  '/student_list/'   #reverse_lazy('list')
+    
+
+
+
+def student_detail(request,id):
+    student = Student.objects.get(id=id)
+    context = {
+        "student":student
+    }
+    
+    return render(request, "fscohort/student_detail.html", context)
+
+class StudentDetailView(DetailView):
+    model = Student
+    pk_url_kwarg = 'id'
+    
+def student_update(request, id):
+    
+    student = Student.objects.get(id=id)
+    
+    form = StudentForm(instance=student)
+    
+    if request.method == "POST":
+        form = StudentForm(request.POST, request.FILES, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+       
+    context= {
+        
+        "student":student,
+        "form":form 
+    }
+    
+    return render(request, "fscohort/student_update.html", context)
+
+class StudentUpdateView(UpdateView):
+    model = Student
+    form_class = StudentForm
+    template_name = "fscohort/student_update.html" #defaul name app/modelname_form.html
+    success_url = reverse_lazy('list') # fonksiyondaki redirect islemini class yapisinda böyle yapiyoruz
+    
+
+def student_delete(request, id):
+    
+    student = Student.objects.get(id=id)
+   
+    if request.method == "POST":
+
+    
+        student.delete()
+        return redirect("list")
+    
+    context= {
+        "student":student
+    }
+    return render(request, "fscohort/student_delete.html",context)
