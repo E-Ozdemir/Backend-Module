456e35bad2b54e019a71144fdc78762d56fde370
E-Ozdemir <emreclarusway@gmail.com>
2021-11-01 19:45:52 +0100

    VisdorApp First Create

---
+++ Backend Module/Projects/VisdorApp/home/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.db.models.fields import CharField
+
+# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=250)
+    email = models.EmailField(max_length=254)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add= True)
+    
+    
+    def __str__(self):
+        return self.name
