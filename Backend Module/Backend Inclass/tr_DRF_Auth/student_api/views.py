2f302f339cade739ec63e84751d44c713f90c419
E-Ozdemir <emreclarusway@gmail.com>
2021-11-26 20:41:17 +0100

    first commit

---
+++ Backend Module/Backend Inclass/tr_DRF_Auth/student_api/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import HttpResponse
+from rest_framework import permissions
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+from .permissions import IsAddedByUserOrReadOnly,IsAdminOrReadOnly
+
+# Create your views here.
+def home(request):
+    return HttpResponse('<h1>API Page</h1>')
+
+class StudentList(generics.ListCreateAPIView):
+    serializer_class = StudentSerializer
+    queryset = Student.objects.all()
+    # permission_classes = [IsAdminOrReadOnly]
+    def perform_create(self, serializer):
+        serializer.save(user= self.request.user)
+
+class StudentOperations(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = StudentSerializer
+    queryset = Student.objects.all()
+    permission_classes = [IsAddedByUserOrReadOnly]
