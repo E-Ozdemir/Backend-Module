ab3228f8e003eb9f592665af6f05c284e8cf9710
E-Ozdemir <emreclarusway@gmail.com>
2021-11-26 22:05:59 +0100

    gender type edit

--- Backend Module/Backend Inclass/18.10-CRUD/django_crud/fscohort/views.py
+++ Backend Module/Backend Inclass/18.10-CRUD/django_crud/fscohort/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .forms import StudentForm
 from .models import Student
+
 # Create your views here.
 
 def home(request):
